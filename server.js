23b7a0973808d340c48efe68f08fb666637be27b
khoa <quockhoa1911@gmail.com>
2022-03-31 18:50:22 +0700

    update

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require('express')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
-const configViewEngine = require('./app/config/viewEngine')
 const cors = require('cors')
-//const PlayerRouter = require('./app/example/routers/player.routers')
-const fruitRouter = require('./app/Fruits/fruit.router')
-
-const db = require('./app/config/db.config')
+const LogRouter = require('../Fruit_api/app/Log/log.router');
+const fruitRouter = require('../Fruit_api/app/Fruits/fruit.router')
+const accountRouter = require("../Fruit_api/app/Account/account.router")
+var session = require('express-session')
+var cookieParser = require('cookie-parser')
+const db = require('../Fruit_api/app/config/db.config')
+const { route } = require('../Fruit_api/app/Fruits/fruit.router')
 db.connect();
+
 
 //access control allow another domain
 const app = express()
@@ -15,6 +18,13 @@ app.use(morgan('dev'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
+
+session = require('express-session');
+app.use(session({
+    secret: '2C44-4D44-WppQ38S',
+    resave: true,
+    saveUninitialized: true
+}));
 const corsOption = {
     "origin": "*",
     "methods": "GET,PUT,POST,DELETE",
@@ -28,7 +38,21 @@ const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
     console.log('server is running on port :' + PORT)
 })
-//configViewEngine(app)
+const auth = function (req, res, next) {
+    if (req.session.role == "admin") {
+        next()
+    } else {
+        res.status(500).json({ message: "server error" })
+    }
+}
+app.use('/login', LogRouter);
+// Logout endpoint
+app.get('/logout', function (req, res) {
+    req.session.destroy();
+    res.send("logout success!");
+});
 
-//app.use('/api/player', PlayerRouter)
-app.use('/api/product', fruitRouter)
+
+app.use('/api/product', auth, fruitRouter)
+
+app.use("/api/account", accountRouter)
